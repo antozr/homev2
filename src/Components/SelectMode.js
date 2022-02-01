d350397d06e42c8a6bfbaa7573ca88d1526f4e90
anto__dev <anto__dev@users.noreply.github.com>
2022-02-01 16:04:07 +0100

    ADD the controle select link

---
+++ src/Components/SelectMode.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+function SelectMode() {
+
+    const dataLink = [
+        {
+            link: "/project-scolaire",
+            titre: "Projet Scolaire"
+        },
+        {
+            link: "/project-perso",
+            titre: "Projet Perso"
+        },
+        {
+            link: "/project-devchallenge",
+            titre: "Projet DevChallenges.io"
+        },
+        {
+            link: "/about",
+            titre: "About"
+        },
+    ];
+
+    return (
+
+
+
+        <div className=" home__select">
+            {dataLink.map((titre) => (
+                <Link to={titre.link} className="home__selctBox">
+                    <h2 className="home__title home__title--select">
+                        {titre.titre}
+                    </h2>
+                    <div className="select__box">
+                        <p className="home__txt">
+
+                            Voir les projets
+                        </p>
+                        <svg width="58" height="10" viewBox="0 0 58 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M50 1.80425C50 1.01881 50.864 0.539969 51.53 0.956249L56.6432 4.152C57.2699 4.54367 57.2699 5.45633 56.6432 5.848L51.53 9.04375C50.864 9.46003 50 8.98119 50 8.19575V1.80425Z" fill="#333333" />
+                            <rect y="4" width="51" height="2" rx="1" fill="#333333" />
+                        </svg>
+                    </div>
+                </Link>
+            ))}
+
+        </div>
+    )
+}
+
+export default SelectMode;
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,13 +1,75 @@
 import React from 'react';
+import SelectMode from '../Components/SelectMode';
+import imageMars from '../assets/img/mars.png';
+
+import './Home.scss';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return (
-      <div className="home">
-         <p>
-             Home bae
-         </p>
-      </div>
-  )
+    return (
+        <div className="home">
+            <div className="home__head">
+                <h1 className="home__title">
+                    Hello 👋 bienvenus dans mon univers
+                </h1>
+            </div>
+            <div className="home__cont">
+                <h2 className="home__title home__title--2">
+                    Moi c’est anto aka anto_dev
+                </h2>
+                <p className="home__txt">
+                    Je suis un étudiant en web dev / design en DWT
+                    ( à l’HEAJ ).
+                    <br />
+                    Tu vas pouvoir retrouver mon portfolio avec mes projets les plus fou comme les plus scolaires, en passant par les bases du web-dev à l’utilisation de framwork  et des petites applications personelle .
+                </p>
+                <p className="home__title home__title--right">
+                    Bonne exploration cher&nbsp;visiteur.
+                </p>
+            </div>
+
+            <SelectMode />
+
+            <div className="home__cont home__cont--rapide">
+                <h2 className="home__title home__title--rapid">
+                    Une envie rapide ?
+                </h2>
+                <div className="home__rapidBox">
+                    <div className="card">
+                        <img src={imageMars} alt="Voici la représentation de mon projet Mars the new hope" className="card__img" />
+                        <h2 className="card__title">
+                        Mars, the new hope
+                        </h2>
+                        <p className="card__txt">
+                        Une page de présentation de la planète rouge, avec ses engins spaciaux humains envoyés des quatres coins du monde. Ainsi on peut voir tout ce qu’il s’y passe ainsi qu’un joli roover.
+                        </p>
+                        <div className="card__list">
+                            <li className="card__el">
+                                <p className="card__tagTxt">
+                                    projet perso 
+                                </p>
+                            </li>
+                            <li className="card__el">
+                                <Link to={"/"} className='card__link'>
+                                    see project
+                                </Link>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
+// data to json 
+/*{
+    "id": 0,
+    "Key":0,
+    "tag":["", " "],
+    "imgPoster" : "",
+    "title" :"",
+    "intro" :" "
+
+}*/
 export default Home;
